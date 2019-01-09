85f6f8a5f72dd627171b14872724b2d2ff98bb25
John Wass <wassj@ctc.com>
2019-01-09 00:14:07 -0500

    kitti fs util

--- kitti/kitti_fs.sh
+++ kitti/kitti_fs.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
-readonly root_path="${1:-${PWD}}"
+readonly root_path="${BASE:-${PWD}}"
 
-mkdir -p ${root_path}/train/images \
-         ${root_path}/train/labels \
-         ${root_path}/val/images   \
-         ${root_path}/val/labels
+
+clean() {
+  rm ${root_path}/train/images/* \
+     ${root_path}/train/labels/* \
+     ${root_path}/val/images/*   \
+     ${root_path}/val/labels/*
+}
+
+make() {
+  mkdir -p ${root_path}/train/images \
+           ${root_path}/train/labels \
+           ${root_path}/val/images   \
+           ${root_path}/val/labels
+}
+
+"$@"
