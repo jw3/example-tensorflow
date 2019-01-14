9930d2825ba3cb4c643548cc6b39e19e52d609ea
John Wass <wassj@ctc.com>
2019-01-14 13:50:01 -0500

    fixes to simrdwn

--- yolo/build_simrdwn.sh
+++ yolo/build_simrdwn.sh
@@ -15,8 +15,9 @@ cd ${raid}/simrdwn/docker
 
 nvidia-docker build --no-cache --build-arg http_proxy --build-arg https_proxy -t simrdwn .
 
-nvidia-docker run -d --name simrdwn_train -v ${raid}:/raid simrdwn bash -c "while true; do sleep 1000; done"
+nvidia-docker run -d --name simrdwn_train -v ${raid}:/raid simrdwn bash -c '''while true; do sleep 1000; done'''
 
-docker exec -w /opt/tensorflow-models git am < ${raid}/0001-fix-exporter.patch
+docker exec simrdwn_train git config --global user.email 'foo@bar.baz'
+docker exec -w /opt/tensorflow-models simrdwn_train bash -c '''git am < /raid/0001-fix-exporter.patch'''
 
 docker exec -w /raid/simrdwn/yolt simrdwn_train make -j8
