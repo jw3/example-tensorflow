fabf10bafa9994f617851fdeced05582b49e8e76
John Wass <wassj@ctc.com>
2019-01-16 14:30:27 -0500

    externalize raid dir

--- yolo/make-tfrecord.sh
+++ yolo/make-tfrecord.sh
@@ -2,8 +2,10 @@
 
 # https://github.com/CosmiQ/simrdwn#1b-create-tfrecord
 
-python /raid/simrdwn/core/preprocess_tfrecords.py \
-    --pbtxt_filename /raid/xview_yolo.pbtxt \
-    --image_list_file /raid/training_list.txt \
-    --outfile /raid/simrdwn/data/xview.tfrecord \
+readonly raiddir="${RAIDDIR:raid}"
+
+python /${raiddir}/simrdwn/core/preprocess_tfrecords.py \
+    --pbtxt_filename /${raiddir}/xview_yolo.pbtxt \
+    --image_list_file /${raiddir}/training_list.txt \
+    --outfile /${raiddir}/simrdwn/data/xview.tfrecord \
     --val_frac 0.0
