3ab55dbb0656c6c5a708d6e205cef18a150d04f2
John Wass <wassj@ctc.com>
2019-01-16 22:36:00 -0500

    fix up the build script for rebuilds

--- yolo/build_simrdwn.sh
+++ yolo/build_simrdwn.sh
@@ -7,7 +7,9 @@ if [[ ! -f ${weights} ]]; then
   curl -o ${weights} https://pjreddie.com/media/files/yolov2.weights
 fi
 
-git clone https://github.com/CosmiQ/simrdwn ${raid}/simrdwn
+if [[ ! -d ${raid}/simrdwn ]]; then
+  git clone https://github.com/CosmiQ/simrdwn ${raid}/simrdwn
+fi
 
 cp ${weights} ${raid}/simrdwn/yolt/input_weights
 
@@ -21,3 +23,10 @@ docker exec simrdwn_train git config --global user.email 'foo@bar.baz'
 docker exec -w /opt/tensorflow-models simrdwn_train bash -c '''git am < /raid/0001-fix-exporter.patch'''
 
 docker exec -w /raid/simrdwn/yolt simrdwn_train make -j8
+
+docker kill simrdwn_train
+docker commit --change='CMD []' simrdwn_train simrdwn:train
+docker rm simrdwn_train
+
+echo "image simrdwn:train is ready!  run it with:"
+echo "nvidia-docker run -d --name simrdwn_train -v $PWD:/raid simrdwn:train /raid/train.sh"
